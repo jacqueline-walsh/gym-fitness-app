442a03ac8556c0caa4cc7336e2d688cc1b444b51
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2024-06-20 20:20:31 +0100

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Box, Stack } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
+
+const Footer = () => (
+  <Box mt="120px" bgcolor="#FFF3F4">
+    <Stack
+      gap="40px"
+      sx={{ alignItems: 'center' }}
+      flexWrap="wrap"
+      px="40px"
+      pt="24px"
+    >
+      <img
+        src={Logo}
+        alt="logo"
+        style={{ width: '200px', height: '61px', paddingBottom: '20px' }}
+      />
+    </Stack>
+  </Box>
+);
+
+export default Footer;
